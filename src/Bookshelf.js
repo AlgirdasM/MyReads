553b08c54c40cbb23b7cd45b1cf4f4529a56359d
AlgirdasM <algirdaslt@gmail.com>
2018-06-29 13:48:37 +0300

    feat: handle authors and images

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -15,7 +15,7 @@ class Bookshelf extends Component {
 						      <li key={book.id}>
 						        <div className="book">
 						          <div className="book-top">
-						            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+						            <div className="book-cover" style={ book.imageLinks && ({ backgroundImage: `url("${book.imageLinks.thumbnail}")` }) }></div>
 						            <div className="book-shelf-changer">
 
 						              <select value={book.shelf} onChange={(e) => update(book, e.target.value)}>
@@ -29,7 +29,7 @@ class Bookshelf extends Component {
 						            </div>
 						          </div>
 						          <div className="book-title">{book.title}</div>
-						          {book.authors.map( author => <div key={author} className="book-authors">{author}</div>)}
+						          {book.authors && (book.authors.map( author => <div key={author} className="book-authors">{author}</div>))}
 						        </div>
 						      </li>
 					      )
@@ -47,7 +47,7 @@ class Bookshelf extends Component {
 						      <li key={book.id}>
 						        <div className="book">
 						          <div className="book-top">
-						            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+						            <div className="book-cover" style={{ backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
 						            <div className="book-shelf-changer">
 
 						              <select value={book.shelf} onChange={(e) => update(book, e.target.value)}>
@@ -61,7 +61,7 @@ class Bookshelf extends Component {
 						            </div>
 						          </div>
 						          <div className="book-title">{book.title}</div>
-						          {book.authors.map( author => <div key={author} className="book-authors">{author}</div>)}
+						          {book.authors && (book.authors.map( author => <div key={author} className="book-authors">{author}</div>))}
 						        </div>
 						      </li>
 					      )
@@ -79,7 +79,7 @@ class Bookshelf extends Component {
 						      <li key={book.id}>
 						        <div className="book">
 						          <div className="book-top">
-						            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+						            <div className="book-cover" style={{ backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
 						            <div className="book-shelf-changer">
 
 						              <select value={book.shelf} onChange={(e) => update(book, e.target.value)}>
@@ -93,7 +93,7 @@ class Bookshelf extends Component {
 						            </div>
 						          </div>
 						          <div className="book-title">{book.title}</div>
-						          {book.authors.map( author => <div key={author} className="book-authors">{author}</div>)}
+						          {book.authors && (book.authors.map( author => <div key={author} className="book-authors">{author}</div>))}
 						        </div>
 						      </li>
 					      )
--- src/Search.js
+++ src/Search.js
@@ -66,7 +66,8 @@ class Search extends Component {
                     <li key={book.id}>
                       <div className="book">
                         <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                          <div className="book-cover" style={ book.imageLinks && ({ backgroundImage: `url("${book.imageLinks.thumbnail}")` }) }>
+                          </div>
                           <div className="book-shelf-changer">
 
                             <select value={book.shelf} onChange={(e) => update(book, e.target.value)}>
